6ad6f79ad8e938e246c69136026a16cd0691a4cc
Etienne Gauthier <egauthier@cymansys-inc.com>
2025-06-29 18:38:45 -0400

    Setting up some base for the new integration

---
+++ src/CommunityToolkit.Aspire.Hosting.Supabase/SupabaseResource.cs
@@ -0,0 +1,15 @@
+﻿
+namespace Aspire.Hosting.ApplicationModel;
+
+/// <summary>
+/// 
+/// </summary>
+/// <param name="name"></param>
+/// <param name="supabaseUrl"></param>
+/// <param name="supabaseKey"></param>
+public class SupabaseResource(string name, string supabaseUrl, string supabaseKey): Resource(name)
+{
+    internal string SupabaseUrl { get; } = supabaseUrl;
+    
+    internal string SupabaseKey { get; } = supabaseKey;
+}
